638c0a7b4a6a829a0fdfb0e899ebe175d29a54ed
Damir Koblev <101816707+TechnoDamo@users.noreply.github.com>
2024-05-06 03:18:46 +0300

    Create database.php

---
+++ HW5/database.php
@@ -0,0 +1,13 @@
+<?php
+require 'pass.php';
+
+$mysqli = new mysqli(hostname: $host,
+    username: $username,
+    password: $password,
+    database: $dbname);
+
+if ($mysqli->connect_errno) {
+    die("Connection error: " . $mysqli->connect_error);
+}
+
+return $mysqli;
